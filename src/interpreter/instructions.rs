8ad9bd75365af9e997b71f99361d824c85c9cefc
fravian99 <fravian99@hotmail.es>
2023-11-27 15:01:57 +0100

    Add instructions

--- src/interpreter/instructions.rs
+++ src/interpreter/instructions.rs
@@ -1,21 +1,27 @@
 #[derive(PartialEq, Eq, Debug)]
 pub enum Instruction {
-    // TODO Implement the other instructions
     IncrementPointer(u32),
     DecrementPointer(u32),
     IncrementValue(u8),
     DecrementValue(u8),
+    Output,
+    Input,
+    OpenLoop,
+    CloseLoop,
     None,
 }
 
 impl From<&char> for Instruction {
     fn from(value: &char) -> Self {
-        // TODO Implement match cases for the other instructions
         match value {
             '>' => Instruction::IncrementPointer(1),
             '<' => Instruction::DecrementPointer(1),
             '+' => Instruction::IncrementValue(1),
             '-' => Instruction::DecrementValue(1),
+            '.' => Instruction::Output,
+            ',' => Instruction::Input,
+            '[' => Instruction::OpenLoop,
+            ']' => Instruction::CloseLoop,
             _ => Instruction::None,
         }
     }
@@ -30,8 +36,10 @@ mod tests {
         assert_eq!(Instruction::from(&'<'), Instruction::DecrementPointer(1));
         assert_eq!(Instruction::from(&'+'), Instruction::IncrementValue(1));
         assert_eq!(Instruction::from(&'-'), Instruction::DecrementValue(1));
+        assert_eq!(Instruction::from(&'.'), Instruction::Output);
+        assert_eq!(Instruction::from(&','), Instruction::Input);
+        assert_eq!(Instruction::from(&'['), Instruction::OpenLoop);
+        assert_eq!(Instruction::from(&']'), Instruction::CloseLoop);
         assert_eq!(Instruction::from(&'?'), Instruction::None);
-        // TODO Implement the other cases
-        todo!();
     }
 }
